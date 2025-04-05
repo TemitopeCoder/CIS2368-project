ed95afe66e0a6eedd37d98f4c928d16c1513cd1d
Mariam Azeez <Temitopeazeez2001@gmail.com>
2025-04-05 18:06:56 -0500

    iput all api files together

--- Backend/Borrowingbooks_RestAPI.py
+++ Backend/Borrowingbooks_RestAPI.py
@@ -7,6 +7,137 @@ Debug = True
 
 conn = create_connection()
 
+# Books Inventory
+
+@app.route('/api/books/inventory', methods=['GET'])
+def view_books():
+    query = "SELECT * FROM books"
+    books = execute_read_query(conn, query)
+    return jsonify(books)
+
+@app.route('/api/books/add', methods=['POST'])
+def add_books():
+    data = request.get_json()
+    required_fields = ["title", "author", "genre", "status"]
+
+    if not all([field in data for field in required_fields]):
+        return jsonify({"error": "Missing data"}), 402
+    
+    add_title = data["title"]
+    add_author = data["author"]
+    add_genre = data["genre"]
+    add_status = data["status"] 
+
+    query = "INSERT INTO books (title, author, genre, status) VALUES (%s, %s, %s, %s)"
+    values = (add_title, add_author, add_genre, add_status)  
+
+    execute_query(conn, query, values)
+    return jsonify({"Message": "New book added"}), 202
+
+@app.route('/api/books/update', methods=['PUT'])
+def update_status():
+    data = request.get_json()
+    required_fields = ["status", "bookid", "title", "author", "genre"]
+
+    if not all([field in data for field in required_fields]):
+        return jsonify({"error": "Missing data"}), 402
+
+    update_status = data["status"]
+    update_id = data["bookid"]
+    update_title = data["title"]
+    update_author = data["author"]
+    update_genre = data["genre"]
+
+    query = "UPDATE books SET status = %s, title = %s, author = %s, genre = %s WHERE bookid = %s"
+    values = (update_status, update_title, update_author, update_genre, update_id)
+
+    execute_query(conn, query, values)
+    return jsonify({"Message": "Book updated"}), 203
+
+@app.route('/api/books/delete/', methods=['DELETE'])
+def delete_book():
+    data = request.get_json()
+    required_fields = ["id"]
+
+    if not all([field in data for field in required_fields]):
+        return jsonify({"error": "Missing data"}), 402
+
+    delete_id = data["id"]
+
+    query = "DELETE FROM books WHERE bookid = %s"
+    values = (delete_id,)
+
+    execute_query(conn, query, values)
+    return jsonify({"Message": "Book deleted"}), 202
+
+# Customers
+
+def hash_password(password):
+    return hashlib.sha256(password.encode('utf-8')).hexdigest()
+
+@app.route('/api/customer/all', methods=['GET'])
+def all_customers():
+    query = "SELECT customerid, firstname, lastname, email FROM customers"
+    customers = execute_read_query(conn, query)
+    return jsonify(customers)
+
+@app.route('/api/customer/add', methods=['POST'])
+def add_customer():
+    data = request.get_json()
+    required_fields = ["email", "firstname", "lastname", "passwordhash"]
+    if not all([field in data for field in required_fields]):
+        return jsonify({"error": "Missing data"}), 402
+
+    add_email = data["email"]
+    add_firstname = data["firstname"]
+    add_lastname = data["lastname"]
+    add_passwordhash = hash_password(data["passwordhash"])
+
+    query = "INSERT INTO customers (email, firstname, lastname, passwordhash) VALUES (%s, %s, %s, %s)"
+    values = (add_email, add_firstname, add_lastname, add_passwordhash)
+
+    execute_query(conn, query, values)
+    return jsonify({"Message": "New customer added"}), 202
+
+@app.route('/api/customer/update/', methods=['PUT'])
+def update_customer():
+    data = request.get_json()
+    required_fields = ["email", "customerid", "firstname", "lastname", "passwordhash"]
+    if not all([field in data for field in required_fields]):
+        return jsonify({"error": "Missing data"}), 402
+
+    update_email = data["email"]
+    update_id = data["customerid"]
+    update_firstname = data["firstname"]
+    update_lastname = data["lastname"]
+    update_passwordhash = hash_password(data["passwordhash"])
+
+    query = "UPDATE customers SET email = %s, firstname = %s, lastname = %s, passwordhash = %s WHERE customerid = %s"
+    values = (update_email, update_firstname, update_lastname, update_passwordhash, update_id)
+
+    execute_query(conn, query, values)
+
+    return jsonify({"Message": "Customer updated"}), 203
+
+@app.route('/api/customer/delete/', methods=['DELETE'])
+def delete_customer():
+    data = request.get_json()
+    required_fields = ["id"]
+    if not all([field in data for field in required_fields]):
+        return jsonify({"error": "Missing data"}), 402
+
+    delete_id = data["id"]
+
+    query = "DELETE FROM customers WHERE customerid = %s"
+    values = (delete_id,)
+
+    execute_query(conn, query, values)
+
+    return jsonify({"Message": "Customer deleted"}), 204
+
+
+# Borrowing Books
+
 @app.route('/api/borrowing', methods=['POST'])
 def books_borrow():
     data = request.get_json()
